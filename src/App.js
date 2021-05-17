213a11cd27cf7ac7c1d7eac6ef872590e5bec6f4
rayalva407 <rayalva407@gmail.com>
2021-05-16 21:40:00 -0600

    adds useEffect

--- src/App.js
+++ src/App.js
@@ -9,6 +9,24 @@ function App() {
   let minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
   let hours = ("0" + Math.floor((time / 3600000) % 60)).slice(-2);
 
+  useEffect(() => {
+    let interval = null;
+
+    if (start) {
+      interval = setInterval(() => {
+        if (time > 0) {
+          setTime(prevTime => prevTime - 10)
+        }
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [start])
+
   return (
     <div className="App">
       <h1>Hello</h1>
