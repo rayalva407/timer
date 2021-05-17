b7976e5c96c638da23c9f2f45293c32742428c35
rayalva407 <rayalva407@gmail.com>
2021-05-16 21:36:15 -0600

    adds state and calculates time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+
+  let seconds = ("0" + (Math.floor((time / 1000) % 60) % 60)).slice(-2);
+  let minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+  let hours = ("0" + Math.floor((time / 3600000) % 60)).slice(-2);
 
   return (
     <div className="App">
